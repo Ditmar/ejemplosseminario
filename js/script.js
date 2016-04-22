0541484ebeb2245eee43365fa74281a2aad9cdc5
Ditmar <ditmarcastro@gmail.com>
2016-04-22 08:49:49 -0400

    Ejemplo seminario

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
-$(function(){
-	
-})();
+$.fn.botones=function()
+{
+	console.log($(this))
+	$(this).mouseover(function(event) {
+		
+		
+		console.log("OVER")
+		$(this).animate({
+			"margin-left":"10px"},
+			500, function() {
+				
+			});
+	});
+	$(this).mouseout(function(event) {
+		console.log("OUT")
+		$(this).animate({
+			"margin-left":"0px"},
+			500, function() {
+				
+			}
+		);
+	});
+}
